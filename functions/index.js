53490fce71f6526ab8c6c1a7a251f958eae15d75
Keshav Bansal <keshavbansal015@gmail.com>
2023-11-30 13:40:13 -0600

    as

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,38 @@
-import { config, firestore } from 'firebase-functions';
-import { initializeApp } from 'firebase-admin';
-import { createTransport } from 'nodemailer';
-initializeApp();
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const functions = require("firebase-functions");
+const config = functions.config;
+const firestore = functions.firestore;
+admin.initializeApp();
 
 // Setting up Nodemailer with SMTP details
-const transporter = createTransport({
-    service: 'gmail', // Replace with your email service
-    auth: {
-        user: config().gmail.email, // Set these in your Firebase config
-        pass: config().gmail.password,
-    },
+const transporter = nodemailer.createTransport({
+  service: "gmail", // Replace with your email service
+  auth: {
+    user: config().gmail.email, // Set these in your Firebase config
+    pass: config().gmail.password,
+  },
 });
 
-export const sendWelcomeEmail = firestore
-    .document('users/{userId}')
-    .onCreate((snap, context) => {
-        const newUser = snap.data();
-        const email = newUser.email; // Assuming email field exists
-        const mailOptions = {
-            from: config().gmail.email, // Replace with your email
-            to: email,
-            subject: 'Welcome to Our App!',
-            text: `Welcome ${newUser.name}! Thank you for joining us.`,
-            // You can also use HTML for the body
-        };
+exports.sendWelcomeEmail = firestore
+  .document("users/{userId}")
+  .onCreate((snap, context) => {
+    const newUser = snap.data();
+    const email = newUser.email; // Assuming email field exists
+    const mailOptions = {
+      from: config().gmail.email, // Replace with your email
+      to: email,
+      subject: "Welcome to Our App!",
+      text: `Welcome ${newUser.name}! Thank you for joining us.`,
+      // You can also use HTML for the body
+    };
 
-        return transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log('Error sending email', error);
-                return false;
-            }
-            console.log('Email sent: ' + info.response);
-            return true;
-        });
+    return transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("Error sending email", error);
+        return false;
+      }
+      console.log("Email sent: " + info.response);
+      return true;
     });
+  });
